71dd9552670bd7241d2e971d0ad0cb522a66f4e8
saeedAhi <saeedahi346@gamil.com>
2026-06-21 13:50:52 +0330

    add mul

--- test.py
+++ test.py
@@ -4,7 +4,12 @@ def div(num1, num2):
     
     return "error"
 
+def mul(num1, num2):
+        return num1 * num2
+
 num1 = int(input("enter first number:"))
 num2 = int(input("enter second number:"))
 
 print(div(num1, num2))
+
+print(mul(num1, num2))
