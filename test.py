72f33cbe53bc15c09bcf14b204626cca2c6d9fc6
saeedAhi <saeedahi346@gamil.com>
2026-06-21 13:52:33 +0330

    add sum

--- test.py
+++ test.py
@@ -4,7 +4,11 @@ def div(num1, num2):
     
     return "error"
 
+def sum(num1, num2):
+    return num1 + num2
+
 num1 = int(input("enter first number:"))
 num2 = int(input("enter second number:"))
 
 print(div(num1, num2))
+print(sum(num1, num2))
